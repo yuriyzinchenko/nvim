023a4cf3332a14bc34b486b8aaba801abba24f17
yurii.zinchenko <yurii.zinchenko@together.com>
2024-10-10 15:53:49 +0200

    telsecope and treesitter configuration

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -1,4 +1,12 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+        { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = "Find files" },
+        { '<leader>fF', function() require('telescope.builtin').find_files({ no_ignore = true, prompt_title = 'All Files' }) end, desc = "Find all files" },
+        { '<leader>fb', function() require('telescope.builtin').buffers() end, desc = "Find buffers" },
+        { '<leader>fg', function() require('telescope.builtit').live_grep() end, desc = "Live grep" },
+        { '<leader>fh', function() require('telescope.builtin').help_tags() end, desc = "Help tags" },
+        { '<leader>fs', function() require('telescope.builtin').lsp_document_symbols() end, desc = "Lsp symbols" },
+  },
 }
--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,96 +1,49 @@
--- Syntax highlighting
-
 return {
-  'nvim-treesitter/nvim-treesitter',
-  event = 'VeryLazy',
-  build = function()
-    require('nvim-treesitter.install').update({ with_sync = true })
-  end,
-  dependencies = {
-    { 'nvim-treesitter/playground', cmd = "TSPlaygroundToggle" },
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  main = 'nvim-treesitter.configs',
-  opts = {
-    ensure_installed = {
-      'bash',
-      'comment',
-      'css',
-      'diff',
-      'dockerfile',
-      'git_config',
-      'git_rebase',
-      'gitattributes',
-      'gitcommit',
-      'gitignore',
-      'go',
-      'html',
-      'http',
-      'ini',
-      'javascript',
-      'json',
-      'jsonc',
-      'lua',
-      'make',
-      'markdown',
-      'passwd',
-      'php',
-      'phpdoc',
-      'python',
-      'regex',
-      'ruby',
-      'rust',
-      'sql',
-      'svelte',
-      'typescript',
-      'vim',
-      'vue',
-      'xml',
-      'yaml',
-    },
-    auto_install = true,
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-      disable = { "yaml" }
-    },
-    context_commentstring = {
-      enable = true,
-    },
-    rainbow = {
-      enable = true,
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ['if'] = '@function.inner',
-          ['af'] = '@function.outer',
-          ['ia'] = '@parameter.inner',
-          ['aa'] = '@parameter.outer',
-        },
-      },
-    },
-  },
-  config = function (_, opts)
-    require('nvim-treesitter.configs').setup(opts)
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
 
-    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = {"src/parser.c"},
-        branch = "main",
-      },
-      filetype = "blade"
-    }
-    vim.filetype.add({
-      pattern = {
-        ['.*%.blade%.php'] = 'blade',
-      },
-    })
-  end,
+      configs.setup({
+          ensure_installed = { 
+              'c', 
+              'lua', 
+              'vim', 
+              'vimdoc', 
+              'query', 
+              'javascript', 
+              'html',
+              'css',
+              'diff',
+              'dockerfile',
+              'git_config',
+              'git_rebase',
+              'gitattributes',
+              'gitcommit',
+              'gitignore',
+              'go',
+              'http',
+              'ini',
+              'json',
+              'jsonc',
+              'make',
+              'markdown',
+              'passwd',
+              'php',
+              'phpdoc',
+              'python',
+              'regex',
+              'ruby',
+              'rust',
+              'sql',
+              'typescript',
+              'vue',
+              'xml',
+              'yaml',
+          },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
 }
