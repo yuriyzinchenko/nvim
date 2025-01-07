16cd37cd8f0dc0cb4212fb653d0eb38ccca2cfe4
yurii.zinchenko <yurii.zinchenko@together.com>
2025-01-07 16:17:13 +0100

    add nvim-telescope

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -2,9 +2,9 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Close all open buffers.
-vim.keymap.set('n', '<leader>Q', ':bufdo bdelete<CR>', { desc = 'Close all open bufferes' })
+--vim.keymap.set('n', '<leader>Q', ':bufdo bdelete<CR>', { desc = 'Close all open bufferes' })
 
 -- Diagnostics.
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [d]iagnostic' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [d]iagnostic' })
+--vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [d]iagnostic' })
+--vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [d]iagnostic' })
 
---
+++ lua/user/plugins/nvim-telescope.lua
@@ -0,0 +1,6 @@
+-- nvim-telescope
+
+return {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+}
